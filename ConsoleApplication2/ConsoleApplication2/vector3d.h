182553723ba1d8f29dafa23ded46821bcb310c6a
Dr Robot <usman.kayaniphd@gmail.com>
2019-12-28 20:16:00 +0000

    new vector3d functions

--- ConsoleApplication2/ConsoleApplication2/vector3d.h
+++ ConsoleApplication2/ConsoleApplication2/vector3d.h
@@ -1,3 +1,5 @@
+#include <cmath>
+
 #pragma once
 namespace mymathlib {
     struct vector3d final {
@@ -39,5 +41,29 @@ namespace mymathlib {
 
     constexpr auto operator-=(vector3d& v1, vector3d const v2) -> vector3d& {
         return v1 = subtract(v1, v2);
+    }
+
+    constexpr auto dot(vector3d const v1, vector3d const v2) -> float {
+        return { v1.x * v2.x + v1.y * v2.y + v1.z * v2.z };
+    }
+
+    constexpr auto cross(vector3d const v1, vector3d const v2) -> vector3d {
+        return { v1.y * v2.z - v1.z * v2.y, v1.x * v2.z - v1.z * v2.x, v1.x * v2.y - v1.y * v2.x };
+    }
+
+    inline auto magnitude(vector3d const v) -> float {
+        return std::sqrt(dot(v, v));
+    }
+
+    constexpr auto scale(vector3d const v, float a, float b, float c) -> vector3d {
+        return { a * v.x, b * v.y, c * v.z };
+    }
+
+    constexpr auto scale(vector3d const v, float a) -> vector3d {
+        return scale(v, a, a, a);
+    }
+
+    auto rotate(vector3d v, float alpha, float beta, float gamma)-> vector3d {
+        return { v.x * cos(beta) * cos(gamma), v.y * cos(alpha) * cos(gamma), v.z * cos(alpha) * cos(beta) };
     }
 }
