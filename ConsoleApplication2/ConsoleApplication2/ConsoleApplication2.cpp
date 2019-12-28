0f75540c71057bcdbe951d1770cc93c95772812f
Dr Robot <usman.kayaniphd@gmail.com>
2019-12-28 16:45:54 +0000

    Adding and substracting vectors

--- ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -5,12 +5,21 @@
 #include <cassert>
 #include "vector3d.h"
 
+namespace ml = mymathlib;
+using namespace std;
+
 int main()
 {
     assert(true);
-    assert(mymathlib::X_HAT.x == 1.f);
-    assert(mymathlib::Y_HAT.y == 1.f);
-    assert(mymathlib::Z_HAT.z == 1.f);
+    auto const testvec = ml::add(ml::X_HAT, ml::Y_HAT);
+    auto const testvec2 = ml::add(testvec, ml::Z_HAT);
+    auto const testvec3 = ml::subtract(testvec2, ml::X_HAT);
+    auto const testvec4 = ml::subtract(testvec3, ml::Y_HAT);
+
+
+    assert(testvec4.x == 0.f);
+    assert(testvec4.y == 0.f); 
+    assert(testvec4.z == 1.f); 
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
--- ConsoleApplication2/ConsoleApplication2/vector3d.h
+++ ConsoleApplication2/ConsoleApplication2/vector3d.h
@@ -8,4 +8,12 @@ namespace mymathlib {
     constexpr auto X_HAT = vector3d{ 1.f, 0.f, 0.f };
     constexpr auto Y_HAT = vector3d{ 0.f, 1.f, 0.f };
     constexpr auto Z_HAT = vector3d{ 0.f, 0.f, 1.f };
+
+    constexpr auto add(vector3d const v1, vector3d const v2) -> vector3d {
+        return { v1.x + v2.x, v1.y + v2.y, v1.z + v2.z };
+    }
+
+    constexpr auto subtract(vector3d const w1, vector3d const w2) -> vector3d {
+        return { w1.x - w2.x, w1.y - w2.y, w1.z - w2.z };
+    }
 }
